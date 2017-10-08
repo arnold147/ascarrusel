e5b3bbb117f730d49a4e5fd59e29cb653d1c4db3
arnold147 <arnold.147@hotmail.com>
2017-10-07 19:11:14 -0500

    Limpiar lineas

--- inc/add-boton.php
+++ inc/add-boton.php
@@ -1,20 +1,17 @@
-<?php 
-
-// Cargar script a la administracion wp
-function wptuts_add_buttons( $plugin_array ) {
-    $plugin_array['bs_grid'] = plugin_dir_url( __FILE__ ) . '../js/as-carrusel-editor.js';
-    return $plugin_array;
-}
-
-function wptuts_register_buttons( $buttons ) {
-    array_push( $buttons, 'bs_grid' ); 
-    return $buttons;
-}
-add_action( 'admin_init', 'wptuts_buttons' );
-function wptuts_buttons() {
-	wp_enqueue_style( 'as-slick_css', plugin_dir_url( __FILE__ ) . '../css/slick-editor.css');  
-    add_filter( "mce_external_plugins", "wptuts_add_buttons" );
-    add_filter( 'mce_buttons_2', 'wptuts_register_buttons' );
-}
-
-
+<?php 
+// Cargar script a la administracion wp
+function ascarrusel_add_buttons( $plugin_array ) {
+    $plugin_array['bs_grid'] = plugin_dir_url( __FILE__ ) . '../js/as-carrusel-editor.js';
+    return $plugin_array;
+}
+
+function ascarrusel_register_buttons( $buttons ) {
+    array_push( $buttons, 'bs_grid' ); 
+    return $buttons;
+}
+add_action( 'admin_init', 'ascarrusel_buttons' );
+function ascarrusel_buttons() {
+	wp_enqueue_style( 'as-slick_css', plugin_dir_url( __FILE__ ) . '../css/slick-editor.css');  
+    add_filter( "mce_external_plugins", "ascarrusel_add_buttons" );
+    add_filter( 'mce_buttons_2', 'ascarrusel_register_buttons' );
+}
